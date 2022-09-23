9884ea12567f2b2659417474aa4dbcce291963d1
monicaliz <monicaeliz3@gmail.com>
2022-09-23 17:23:19 -0400

    commit tree 3aa367e9a1336b4377ab3266593af6d949401ca2

--- src/components/App.js
+++ src/components/App.js
@@ -17,16 +17,30 @@ function Home() {
 }
 
 {/* write an <About> component here */}
-
+function About() {
+  return (
+    <div id="about">
+      <h2>About</h2>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div>
       <NavBar />
       {/* add the <Home> component here */}
+      <Home />
       {/* add your <About> component here */}
+      <About />
     </div>
   );
 }
 
 export default App;
+
+
+
+// DELIVERABLES
+// 1. add the <Navbar> and <Home> components as child components of <App> by including them in the return statement of the App() function 
+// 2. create third child component <About> to be child of <App> and it should return a <div id= "about"> and contain <h2>About</h2>
